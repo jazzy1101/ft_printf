23d6b273cd5ae4da76c8ed98119c3961cbbc2f68
Daram Bae <dabae@c3r1p4.42perpignan.fr>
2023-10-17 17:38:33 +0200

    today's work

--- ft_check_type.c
+++ ft_check_type.c
@@ -1,22 +1,42 @@
 #include "libft.h"
 
-void	ft_check_type(int pos, t_list arg, )
+void	ft_check_type(t_print set, const char *str, int pos)
 {
-	while (!ft_strchr("cspdiuxX%", arg[pos]))
+	while (!ft_strchr("cspdiuxX%", str[pos]))
 	{
-		if (arg[pos] == '.')
+		if (str[pos] == '.')
+		{
+			set->point = 1;
+			pos++;
+		}
+		if (str[pos] == '-')
+		{
+			set->minus = 1;
+			pos++;
+		}
+		if (ft_isdigit(str[pos]) && str[pos] != '0')
+		{
+		if (str[pos] == '0' && ft_isdigit(str[pos + 1]))
+		{
+			
 
-		if (arg[pos] == '-')
+		if (str[pos] == '#')
+		{
+			set->hash = 1;
+			pos++;
+		}
+		if (str[pos] == '+')
 
-		if (arg[pos] == '')
 	}
-	if (arg[pos] == 'c')
-	else if (arg[pos] == 's')
-	else if (arg[pos] == 'p')
-	else if (arg[pos] == 'd' || arg[pos] == 'i')
-	else if (arg[pos] == 'u')
-	else if (arg[pos] == 'x' || arg[pos] == 'X')
-	else if (arg[pos] == '.')
-	else if ()
-
+	if (str[pos] == 'c')
+		ft_print_char(va_arg(set->args, char));
+	else if (str[pos] == 's')
+		ft_print_str(va_arg(set->args, const char*));
+	else if (str[pos] == 'p')
+		ft_print_pointer(va_arg(set->args, void*));
+	else if (str[pos] == 'd' || str[pos] == 'i')
+		ft_print_int(va_arg(set->args, int));
+	else if (str[pos] == 'x' || str[pos] == 'X' || str[pos] == 'u')
+		ft_print_unsign_int(va_arg(set->args, unsigned int));
+	return ();
 }
