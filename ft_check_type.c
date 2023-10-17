bd9b19d5b4ab779a987d04036acb023c00abc647
darambae <64197421+jazzy1101@users.noreply.github.com>
2023-10-17 10:23:03 +0200

    function to check what follows %

---
+++ ft_check_type.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+void	ft_check_type(int pos, t_list arg, )
+{
+	while (!ft_strchr("cspdiuxX%", arg[pos]))
+	{
+		if (arg[pos] == '.')
+
+		if (arg[pos] == '-')
+
+		if (arg[pos] == '')
+	}
+	if (arg[pos] == 'c')
+	else if (arg[pos] == 's')
+	else if (arg[pos] == 'p')
+	else if (arg[pos] == 'd' || arg[pos] == 'i')
+	else if (arg[pos] == 'u')
+	else if (arg[pos] == 'x' || arg[pos] == 'X')
+	else if (arg[pos] == '.')
+	else if ()
+
+}
