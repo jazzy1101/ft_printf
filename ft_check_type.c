9cc423834b4b1070dc68c2f29c804d5804aa316e
Daram Bae <dabae@c3r2p4.42perpignan.fr>
2023-10-18 11:17:50 +0200

    checking type and etc

--- ft_check_type.c
+++ ft_check_type.c
@@ -1,33 +1,22 @@
-#include "libft.h"
 
-void	ft_check_type(t_print set, const char *str, int pos)
+void	ft_check_flag(t_print *set, const char *str, int pos)
 {
 	while (!ft_strchr("cspdiuxX%", str[pos]))
 	{
 		if (str[pos] == '.')
-		{
 			set->point = 1;
-			pos++;
-		}
 		if (str[pos] == '-')
-		{
 			set->minus = 1;
-			pos++;
-		}
-		if (ft_isdigit(str[pos]) && str[pos] != '0')
-		{
-		if (str[pos] == '0' && ft_isdigit(str[pos + 1]))
-		{
-			
-
 		if (str[pos] == '#')
-		{
 			set->hash = 1;
-			pos++;
-		}
 		if (str[pos] == '+')
+			set->plus
 
 	}
+}
+void	ft_check_type(t_print *set, const char *str, int pos)
+{
+	
 	if (str[pos] == 'c')
 		ft_print_char(va_arg(set->args, char));
 	else if (str[pos] == 's')
--- ft_printf.c
+++ ft_printf.c
@@ -15,7 +15,7 @@ t_print	*ft_init_set(t_print *set)
 {
 	set->n = 0;
 	set->width = 0;
-	set->preci = 0;
+	set->preci = -1;
 	set->len = 0;
 	set->zero = 0;
 	set->minus = 0;
@@ -23,27 +23,35 @@ t_print	*ft_init_set(t_print *set)
 	set->space = 0;
 	set->hash = 0;
 	set->point = 0;
-	return (tab);
+	return (set);
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_parse_str(const char *str, t_print *set, va_list ap)
 {
 	int	i;
-	int	res;
-	t_print	*set;
 
-	set = (t_print *)malloc(sizeof(t_print));
-	if (!set)
-		return (-1);
-	ft_init_set(set);
-	va_start(set->args, str);
 	i = -1;
 	while (str[++i])
 	{
 		if (str[i] == '%')
 			ft_check_type(set, str, i);
 		else
-			set->n
+			set->n += write(1, &str[i], 1);
+	}
+	return (set->n);
+}
+int	ft_printf(const char *str, ...)
+{
+	int	res;
+	t_print	*set;
+	va_list	ap;
 
+	res = 0;
+	set = (t_print *)malloc(sizeof(t_print));
+	if (!set)
+		return (-1);
+	ft_init_set(set);
+	va_start(set->args, str);
+	res = ft_parse_str(str, set, ap);
 	
 }
